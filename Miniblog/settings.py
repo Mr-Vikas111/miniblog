36799fb590f8d9fab1f5e7820858e7a6526de3b0
Vikash minj <81070842+Mr-Vikas111@users.noreply.github.com>
2021-08-30 20:53:08 +0530

    setting change

--- Miniblog/settings.py
+++ Miniblog/settings.py
@@ -118,6 +118,5 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
